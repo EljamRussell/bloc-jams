c5eb6d6b17d80c01de0fc79ad5f4d880339368ca
Eljam Sab Russell <eljamrussell@gmail.com>
2017-03-09 22:26:28 -0500

    Update landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -8,7 +8,7 @@
                      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";  
      }         
  
-     for (var i = 0l i < points.length; i++){
+     for (var i = 0; i < points.length; i++){
            revealFirstPoint(i); 
         }
      };
